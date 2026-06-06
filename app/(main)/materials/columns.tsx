042342402cd0d97bb95d2e7840fbf9fe56e934a4
CarlosCastano33 <126597422+CarlosCastano33@users.noreply.github.com>
2026-06-06 17:23:58 -0500

    Refactor Material type to use title and required fields
    Updated Material type to enforce required fields.

--- app/(main)/materials/columns.tsx
+++ app/(main)/materials/columns.tsx
@@ -6,18 +6,17 @@ import { ColumnDef } from '@tanstack/react-table';
 // You can use a Zod schema here if you want.
 export type Material = {
   id: string;
-  name: string;
-
-  author?: string;
+  title: string;
+  author: string;
   description?: string;
-  publisher?: string;
-  isbn?: string;
+  publisher: string;
+  isbn: string;
 
   // Clasificación
   category: string;
   materialType: "BOOK" | "MAGAZINE" | "THESIS" | "DVD" | "EBOOK";
 
-  publicationYear?: number;
+  publicationYear: number;
 
   // Estado
   status: "AVAILABLE" | "BORROWED" | "RESERVED" | "LOST" | "DAMAGED";
