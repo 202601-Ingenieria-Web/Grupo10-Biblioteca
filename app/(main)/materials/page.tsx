01a63e55ca7a5313a6c8d6be301ea3e95f19f70d
Carlos Castaño <carlose.castano@udea.edu.co>
2026-06-06 19:50:34 -0500

    page materials

--- app/(main)/materials/page.tsx
+++ app/(main)/materials/page.tsx
@@ -1,13 +1,13 @@
-import { columns, User } from './columns';
+import { columns, Material } from './columns';
 import { DataTable } from './data-table';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
-async function getData(): Promise<Movement[]> {
-  const response = await fetch('http://localhost:3000/api/movements');
+async function getData(): Promise<Material[]> {
+  const response = await fetch('http://localhost:3000/api/materials');
   const data = await response.json();
 
-  return data.movements;
+  return data.materials;
 }
 
 export default async function DemoPage() {
@@ -18,10 +18,10 @@ export default async function DemoPage() {
       <div className='flex flex-row justify-around my-10 '>
         <div>
           <h2 className='text-2xl font-bold tracking-tight'>Movements</h2>
-          <p className='text-muted-foreground'>Manage your movements here.</p>
+          <p className='text-muted-foreground'>Manage your materials here.</p>
         </div>
-        <Link href='/movements/create'>
-          <Button className='ml-auto'>Create Movement</Button>
+        <Link href='/materials/create'>
+          <Button className='ml-auto'>Create Material</Button>
         </Link>
       </div>
       <DataTable columns={columns} data={data} />
